ab7c7465845181104702b960e7807daad001f401
ejd500 <ellen.dalton-dawe@keyin.com>
2024-07-29 23:37:46 -0230

    initial commit of availability page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './styles/index.css';
 import Home from '../src/pages/Home';
 import Availability from '../src/pages/Availability';
+import { useState } from 'react';
 
 function App() {
+  const [checkInDate, setCheckInDate] = useState(null);
+  const [checkOutDate, setCheckOutDate] = useState(null);
+  const [guests, setGuests] = useState("");
+
   return (
      <BrowserRouter>
        <Routes>
          <Route path="/" element={<Home />} />
-         <Route path="availability" element={<Availability />} />
+         <Route path="availability" element={<Availability checkInDate={checkInDate} setCheckInDate={setCheckInDate} checkOutDate={checkOutDate} setCheckOutDate={setCheckOutDate} guests={guests} setGuests={setGuests}/>} />
        </Routes>
      </BrowserRouter>
   );
