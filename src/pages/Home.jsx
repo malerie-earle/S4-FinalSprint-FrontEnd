0209dcbf9614b639e928a3273d4cda904c5c6cef
Janeil Chantelle <132310787+janeilchantelle@users.noreply.github.com>
2024-07-31 02:09:22 -0230

    updated nav hover, added overlay/text.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,10 @@ const Home = () => {
     <div>
       <Nav />
       <Slideshow />
+      <div className="overlay">
+      <p>I S L A N D&nbsp;&nbsp;&nbsp;S E R E N I T Y</p>
+
+        </div>
       <div className="homepage-content">
         <h1>This is a homepage.</h1>
       </div>
