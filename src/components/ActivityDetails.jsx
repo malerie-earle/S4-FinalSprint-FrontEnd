408d18af0158695ac3c2422ad04f157bb35790c3
ejd500 <ellen.dalton-dawe@keyin.com>
2024-08-02 16:11:59 -0230

    added dynamic content for activities

--- src/components/ActivityDetails.jsx
+++ src/components/ActivityDetails.jsx
@@ -1,31 +1,16 @@
 import "../styles/activity-details.css"
 import parasailing from "../images/parasailing.jpg"
 
-const ActivityDetails = () => {
+const ActivityDetails = ({activity}) => {
+
   return (
     <form className="activity-form">
-      <h3 className="activity-name">Activity Name Here</h3>
+      <h3 className="activity-name">{activity.name}</h3>
       <div className="activity-image-text">
-        <img src={parasailing} alt="Example Activity" className="activityImage"/>
+        <img src={activity.image1} alt="Example Activity" className="activityImage"/>
         <div className="activity-textbox">
-            <p id="activityDescription">Activity description here.</p>
-            <p id="activityDate">DATE: </p>
-            <p id="activityTimes">Please choose your activity time:</p>
-            <div className="activityTimes">
-              <div className="times">
-                <label htmlFor="time1">- Time 1</label>
-                <input type="radio" name="time" id="time1" />
-              </div>
-              <div className="times">
-                <label htmlFor="time2">- Time 2</label>
-                <input type="radio" name="time" id="time2" />
-              </div>
-              <div className="times">
-                <label htmlFor="time3">- Time 3</label>
-                <input type="radio" name="time" id="time3" />
-              </div>
-              
-            </div>
+            <p id="activityDescription">{activity.description}</p>
+            <p id="activityDate">Occurs daily at _____</p>
             <button type="submit">BOOK NOW</button>
         </div>
       </div>
