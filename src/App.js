810987ec8a698fe980d02f1d38d2141088e2f743
Malerie Earle <malerie.augot@gmail.com>
2024-08-05 10:49:57 -0230

    back to d9666f7ae34bf0d1a0d44b3bb0e2f7612a6c28b9

--- src/App.js
+++ src/App.js
@@ -1,83 +1,76 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-import './styles/index.css';
 import Home from './pages/Home';
 import RoomAvailability from './pages/RoomAvailability';
 import ActivityAvailability from './pages/ActivityAvailability';
-import Booking from './pages/Booking';
-import Account from './pages/Account';
-import BookingConfirmation from './pages/BookingConfirmation';
 import useFetch from './hooks/useFetch';
-import usePost from './hooks/usePost';
-import { useState } from 'react';
 
 function App() {
-  const [checkInDate, setCheckInDate] = useState(null);
-  const [checkOutDate, setCheckOutDate] = useState(null);
-  const [guests, setGuests] = useState('');
-  const [activityDate, setActivityDate] = useState(null);
+  const [checkInDate, setCheckInDate] = React.useState(null);
+  const [checkOutDate, setCheckOutDate] = React.useState(null);
+  const [guests, setGuests] = React.useState('');
+  const [activityDate, setActivityDate] = React.useState(null);
 
   const { data: allRoomData, loading: allRoomLoading, error: allRoomError } = useFetch('http://localhost:8080/api/rooms');
   const { data: allActivityData, loading: allActivityLoading, error: allActivityError } = useFetch('http://localhost:8080/api/activities');
-  const { data: activityBookingData, loading: activityBookingLoading, error: activityBookingError, postData: postActivityBookingData } = usePost('http://localhost:8080/api/activities/book');
 
   return (
-      <Routes>
-        {/* Public routes accessible without authentication */}
-        <Route path="/" element={<Home />} />
-        <Route
-          path="/room-availability"
-          element={
-            <RoomAvailability
-              checkInDate={checkInDate}
-              setCheckInDate={setCheckInDate}
-              checkOutDate={checkOutDate}
-              setCheckOutDate={setCheckOutDate}
-              guests={guests}
-              setGuests={setGuests}
-              allRoomData={allRoomData}
-              allRoomLoading={allRoomLoading}
-              allRoomError={allRoomError}
-            />
-          }
-        />
-        <Route
-          path="/activity-availability"
-          element={
-            <ActivityAvailability
-              activityDate={activityDate}
-              setActivityDate={setActivityDate}
-              allActivityData={allActivityData}
-              allActivityLoading={allActivityLoading}
-              allActivityError={allActivityError}
-            />
-          }
-        />
+    <Routes>
+      {/* Public routes accessible without authentication */}
+      <Route path="/" element={<Home />} />
+      <Route
+        path="/room-availability"
+        element={
+          <RoomAvailability
+            checkInDate={checkInDate}
+            setCheckInDate={setCheckInDate}
+            checkOutDate={checkOutDate}
+            setCheckOutDate={setCheckOutDate}
+            guests={guests}
+            setGuests={setGuests}
+            allRoomData={allRoomData}
+            allRoomLoading={allRoomLoading}
+            allRoomError={allRoomError}
+          />
+        }
+      />
+      <Route
+        path="/activity-availability"
+        element={
+          <ActivityAvailability
+            activityDate={activityDate}
+            setActivityDate={setActivityDate}
+            allActivityData={allActivityData}
+            allActivityLoading={allActivityLoading}
+            allActivityError={allActivityError}
+          />
+        }
+      />
 
-        {/* Authenticated routes handled by Authenticator */}
-        <Route
-          path="/*"
-          element={
-            <Authenticator>
-              {({ signOut, user }) => (
+      {/* Authenticated routes handled by Authenticator */}
+      <Route
+        path="/*" // Catch-all for other routes
+        element={
+          <Authenticator>
+            {({ signOut, user }) => (
+              user ? (
                 <Routes>
-                  {user ? (
-                    <>
-                      <Route path="/" element={<Home />} />
-                      <Route path="/booking" element={<Booking />} />
-                      <Route path="/account" element={<Account signOut={signOut} />} />
-                      <Route path="/booking-confirmation" element={<BookingConfirmation />} />
-                    </>
-                  ) : (
-                    <Route path="*" element={<Home />} /> // Redirect to home or a 404 page
-                  )}
+                  <Route path="/" element={<Home />} />
+                  {/* Add other authenticated routes as needed */}
                 </Routes>
-              )}
-            </Authenticator>
-          }
-        />
-      </Routes>
+              ) : (
+                <Routes>
+                  {/* Redirect or handle unauthenticated access */}
+                  <Route path="*" element={<Home />} /> {/* Redirect to home or a 404 page */}
+                </Routes>
+              )
+            )}
+          </Authenticator>
+        }
+      />
+    </Routes>
   );
 }
 
--- src/components/RoomDetails.jsx
+++ src/components/RoomDetails.jsx
@@ -6,6 +6,9 @@ import { useState } from 'react';
 import RoomImageSlideshow from "./RoomImageSlideshow"
 
 const RoomDetails = ({room}) => {
+
+  const description = room.description;
+  const roomName = room.room_name;
   const image1 = room.image1;
   const image2 = room.image2;
   const image3 = room.image3;
@@ -14,19 +17,20 @@ const RoomDetails = ({room}) => {
   imagesList.push(image2);
   imagesList.push(image3);
   console.log(imagesList);
-
+  const occupancy = room.occupancy;
+  const beds = room.beds;
 
   return (
     <div className="roomBookingDetails">
-        <h3 className="room-name">{room.name}</h3>
+        <h3 className="room-name">{roomName}</h3>
         <form className="room-form">
           <RoomImageSlideshow imagesList={imagesList}/>
           <div className="room-textbox">
-              <p className="roomDescription">{room.description}</p>
+              <p className="roomDescription">{description}</p>
               <div className="occupancy-beds">
                 
-                <p className="symbols-box"><img src={person} alt="Guests" className="symbols"/>{room.occupancy}</p>
-                <p className="symbols-box"><img src={bed} alt="Beds" className="symbols"/>{room.beds}</p>
+                <p className="symbols-box"><img src={person} alt="Guests" className="symbols"/>{occupancy}</p>
+                <p className="symbols-box"><img src={bed} alt="Beds" className="symbols"/>{beds}</p>
               </div>
               <button type="submit">BOOK NOW</button>
           </div>
