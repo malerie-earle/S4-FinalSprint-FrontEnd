bffcdbf663fe6b2fe2b0428398025c015a8cbc00
Malerie Earle <malerie.augot@gmail.com>
2024-08-11 22:11:05 -0230

    fixing bugs

--- src/components/ActivityBookings.jsx
+++ src/components/ActivityBookings.jsx
@@ -1,22 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import { getCurrentUser } from '@aws-amplify/auth';
+
 import config from '../config';
 
 import useFetch from '../hooks/useFetch';
 
 const ActivityBookings = ({user, booking}) => {
   console.log(user);
-  const [bookings, setBookings] = useState([]);
-  // const [lerror, setError] = useState(null);
-  const [isFetched, setIsFetched] = useState(false); 
   const {data:activityBookings, loading: activityBookingsLoading, error: activityBookingsError} = useFetch(`${config.backendBaseURL}/api/activities/bookings/username?username=${user.username}`)
 
   console.log(activityBookings)
 
-  const location = useLocation();
-  const date = location.state?.date;
-  const activity = location.state?.activity;
   
 
   return (
@@ -25,7 +17,7 @@ const ActivityBookings = ({user, booking}) => {
       <h1>Activity Bookings</h1>
       {activityBookingsLoading && <p>{activityBookingsLoading}</p>}
       {activityBookingsError && <p style={{ color: 'red' }}>{activityBookingsError}</p>}
-      {!activityBookingsError && !activityBookingsLoading && activityBookings.length==0 && activityBookings != null? 
+      {!activityBookingsError && !activityBookingsLoading && activityBookings.length===0 && activityBookings != null? 
         <p>No activity bookings found.</p>
         :
         (
